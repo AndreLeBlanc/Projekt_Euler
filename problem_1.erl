af2eec8b482792b9e270c791f08af6523d80d351
AndreLeBlanc <andre.leblanc@zoho.com>
2018-01-20 17:00:06 +0100

    tid

--- problem_1.erl
+++ problem_1.erl
@@ -2,5 +2,10 @@
 -export([fiveThree/0]).
 
 fiveThree() ->
+    Start = os:timestamp(),
 	Y = [X || X <- lists:seq(1, 999), (X rem 3 =:= 0) or (X rem 5 =:= 0)],
-	lists:sum(Y).
+	Resultat = lists:sum(Y),
+	io:format("Svaret är ~p och tog ~f sekunder att beräkna~n", [Resultat, timer:now_diff(os:timestamp(), Start) / 1000]).
+
+
+
