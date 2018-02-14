f4f66d3e943cfff8486c673a4096b0b658c28345
André Le Blanc <leblanc_112@hotmail.com>
2018-02-14 10:40:30 +0100

    Update problem_4.erl
    snabblösning

--- problem_4.erl
+++ problem_4.erl
@@ -1,9 +1,20 @@
 -module(problem_4).
--export([problem_4/0]).
+-export([problem_4/0, problem_4_snabbare/0]).
 
 problem_4() ->
     Start = os:timestamp(),
     Listan = lists:seq(100,999),
-    Pallar = [X*Y || X <- Listan, Y <- Listan, integer_to_list(X*Y) == lists:reverse(integer_to_list(X * Y))],
+    Pallar = [X*Y || X <- Listan, Y <- Listan, integer_to_list(X*Y) == lists:reverse(integer_to_list(X*Y))],
     Res = lists:max(Pallar),
     io:format("Resultatet: ~p och tog ~p sekunder att beräkna", [Res, timer:now_diff(os:timestamp(), Start) / 1000000]).
+
+problem_4_snabbare() ->
+    Start = os:timestamp(),
+    Res = lists:max(pall(999)),
+    io:format("Resultatet: ~p och tog ~p sekunder att beräkna", [Res, timer:now_diff(os:timestamp(), Start) / 1000000]).
+
+pall(99) -> [];
+pall(N) ->
+    Listan = lists:seq(100,N),
+    Pallar = [X*N || X <- Listan, integer_to_list(X*N) == lists:reverse(integer_to_list(X*N))],
+    Pallar ++ pall(N-1).
