dda50d4cf4c4921fbf252376a938fe3f9c95925e
AndreLeBlanc <leblanc_112@hotmail.com>
2018-04-19 15:20:54 +0200

    lade problem 52

---
+++ problem_52.erl
@@ -0,0 +1,26 @@
+-module(problem_52).
+-export([problem_52/0, perms/1]).
+
+problem_52() ->
+    Start = os:timestamp(),
+    Res = problem_52_helper(100002),
+    io:format("Resultatet: ~p och det tog ~p sekunder att beräkna~n", [Res, timer:now_diff(os:timestamp(), Start)/1000000]).
+
+problem_52_helper(A) ->
+    A_list = integer_to_list(A),
+    IsPerm  = [member(integer_to_list(A*X), A_list) || X <- lists:seq(2, 6)],
+    case lists:member(false, IsPerm) of
+	true -> problem_52_helper(A+3);
+	false -> A
+    end.	    
+
+member([], _) -> true;
+member([H|T], A) ->
+    case lists:member(H, A) of
+	true -> member(T, A);
+	false -> false
+    end. 
+
+perms([]) -> [[]];
+perms(L) -> [[H|T] || H <- L, T <- perms(L--[H])].
+
