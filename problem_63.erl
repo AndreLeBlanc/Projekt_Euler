f7139031fd740b16f2d5a44b635a0fb0e1609ec7
AndreLeBlanc <leblanc_112@hotmail.com>
2018-04-20 15:59:10 +0200

    lade till problem 63

---
+++ problem_63.erl
@@ -0,0 +1,17 @@
+-module(problem_64).
+-export([problem_63/0]).
+
+problem_63() ->
+    Start = os:timestamp(),
+    Powers = [pow(X, Y) || X <- lists:seq(1, 10), Y <- lists:seq(0, 100), powlength(X, Y)],
+    Resultat = length(Powers),
+    io:format("Svaret är ~p och tog ~f sekunder att berakna~n", [Resultat, timer:now_diff(os:timestamp(), Start) / 1000000]).
+
+powlength(X, Y) ->
+    Power = pow(X, Y),
+    Listified = integer_to_list(Power),
+    length(Listified) == Y.
+	    
+pow(_, 0) -> 1;
+pow(X, Y) -> X * pow(X, Y-1).
+     
