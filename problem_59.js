e80f952ac8e01d4c8e46932752667f70da870e10
AndreLeBlanc <leblanc_112@hotmail.com>
2019-11-07 17:31:33 +0100

    cleaned up code

--- problem_59.js
+++ problem_59.js
@@ -1,48 +1,48 @@
 const performance = require('perf_hooks').performance;
-function readr() {
-    var lines = require('fs').readFileSync('txtfiles/p059_cipher.txt', 'utf-8');
-    return lines.split(',');
+function readr () {
+  var lines = require('fs').readFileSync('txtfiles/p059_cipher.txt', 'utf-8');
+  return lines.split(',');
 }
 
-function isEnglish(outPut) {
-    for(j = 0; j < outPut.length-2; j++) {
-        if(outPut[j] === 116 && outPut[j+1] === 104 && outPut[j+2] === 101){
-            for (let k = 0; k < outPut.length-3; k++) {
-                if(outPut[k] === 109 && outPut[k+1] === 111 && outPut[k+2] === 115 && outPut[k+3] === 116){
-                    return outPut.reduce((a, b) => a + b, 0);
-                }
-            }
+function isEnglish (outPut) {
+  for (let j = 0; j < outPut.length - 2; j++) {
+    if (outPut[j] === 116 && outPut[j + 1] === 104 && outPut[j + 2] === 101) {
+      for (let k = 0; k < outPut.length - 3; k++) {
+        if (outPut[k] === 109 && outPut[k + 1] === 111 && outPut[k + 2] === 115 && outPut[k + 3] === 116) {
+          return outPut.reduce((a, b) => a + b, 0);
         }
+      }
     }
-    return -1;
- }
+  }
+  return -1;
+}
 
-function correctKey(key, lines) {
-    var output = [];
-    for (let i = 0; i < lines.length; i++) {
-        let x = lines[i] ^ key[i%3];
-        output.push(x);
-     }
+function correctKey (key, lines) {
+  var output = [];
+  for (let i = 0; i < lines.length; i++) {
+    let x = lines[i] ^ key[i % 3];
+    output.push(x);
+  }
 
-    return isEnglish(output);
+  return isEnglish(output);
 }
 
 function problem_59() {
-    var lines = readr();
-    var start = performance.now();
-    for (let i = 97; i <= 122; i++) {
-        for (let j = 97; j <= 122; j++) {
-            for (let k = 97; k <= 122; k++){
-                let key = [i, j, k];
-                ans = correctKey(key, lines);
-                if (0 < ans) {
-                    var end = performance.now();
-                    console.log("The answer is:", ans, "and it took,", end-start, "to calculate");
-                    return 0;
-                }
-            }
+  var lines = readr();
+  var start = performance.now();
+  for (let i = 97; i <= 122; i++) {
+    for (let j = 97; j <= 122; j++) {
+      for (let k = 97; k <= 122; k++){
+        const key = [i, j, k];
+        const ans = correctKey(key, lines);
+        if (ans > 0) {
+          var end = performance.now();
+          console.log("The answer is:", ans, "and it took,", end - start, "to calculate");
+          return 0;
         }
+      }
     }
+  }
 }
 
 problem_59();
--- problem_79.js
+++ problem_79.js
@@ -1,46 +1,46 @@
 const performance = require('perf_hooks').performance;
-function keyList(){
-    var lines = require('fs').readFileSync('txtfiles/keylog.txt', 'utf-8')
-        .split('\n')
-        .filter(Boolean);
-    return [...new Set(lines)];
+function keyList () {
+  var lines = require('fs').readFileSync('txtfiles/keylog.txt', 'utf-8')
+    .split('\n')
+    .filter(Boolean);
+  return [...new Set(lines)];
 }
 
-function bef() {
-    const keys = keyList();
-    const start = performance.now();
-    var befores = [[],[],[],[],[],[],[],[],[],[]];
-    const keySplit = keys.map(i => i.split('').map(Number));
-    for (let i = 0; i < keySplit.length; i++) {
-        befores[keySplit[i][2]].push(keySplit[i][1]);
-        befores[keySplit[i][2]].push(keySplit[i][0]);
-        befores[keySplit[i][1]].push(keySplit[i][0]);
-    }
-    befores = befores.map(x => [...new Set(x)]);
-    befores.sort(function(a, b){
-        return a.length - b.length;
-    })
-    return { befores, start };
+function bef () {
+  const keys = keyList();
+  const start = performance.now();
+  var befores = [[], [], [], [], [], [], [], [], [], []];
+  const keySplit = keys.map(i => i.split('').map(Number));
+  for (let i = 0; i < keySplit.length; i++) {
+    befores[keySplit[i][2]].push(keySplit[i][1]);
+    befores[keySplit[i][2]].push(keySplit[i][0]);
+    befores[keySplit[i][1]].push(keySplit[i][0]);
+  }
+  befores = befores.map(x => [...new Set(x)]);
+  befores.sort(function (a, b) {
+    return a.length - b.length;
+  })
+  return { befores, start };
 }
 
-function problem_79() {
-    const { befores, start } = bef();
-    var res = [];
-    for (let i = 0; i < befores.length; i++) {
-        if (befores[i].length > 0) {
-            let digit = befores[i][0];
-            res.push(digit);
-            for (let j = i; j < befores.length; j++) {
-                let index = befores[j].indexOf(digit);
-                befores[j].splice(index, 1);
-            }
-        }
+function problem_79 () {
+  const { befores, start } = bef();
+  var res = [];
+  for (let i = 0; i < befores.length; i++) {
+    if (befores[i].length > 0) {
+      const digit = befores[i][0];
+      res.push(digit);
+      for (let j = i; j < befores.length; j++) {
+        const index = befores[j].indexOf(digit);
+        befores[j].splice(index, 1);
+      }
     }
-    res.push(0);
-    res = parseInt(res.join(''));
-    const finish = performance.now();
-    console.log(res);
-    console.log(finish-start);
+  }
+  res.push(0);
+  res = parseInt(res.join(''));
+  const finish = performance.now();
+  console.log(res);
+  console.log(finish - start);
 }
 
 problem_79();
